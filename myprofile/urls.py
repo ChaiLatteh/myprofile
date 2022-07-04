81421a38dce0e0ad3a7dde4d3671f510f4b8a9f2
Chai Jang <wkdcodns100@gmail.com>
2022-07-04 16:26:09 -0700

    updates

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('home.urls')),
+    path('tinymce', include('tinymce.urls')),
     path('crave/', include('crave.urls')),
     path('holywave/', include('holywave.urls')),
     path('admin/', admin.site.urls),
